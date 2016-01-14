f57ae4bdc5ff11de31efa45c12fdbbf28674bc43
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-01-14 15:52:43 +0100

    use new job params

--- get_logs.sh
+++ get_logs.sh
@@ -12,21 +12,21 @@ if [ -d logs ]; then
 fi
 
 mkdir logs
-count=0
 for h in $(ansible/inventory.py --hosts nuxeo); do
     mkdir logs/$h
     scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$h:/opt/nuxeo/logs/server.log logs/$h/ || true
     scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$h:/opt/nuxeo/logs/gc.log logs/$h/ || true
     scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$h:/opt/nuxeo/server/nxserver/perf*.csv logs/$h/ || true
     scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$h:/opt/nuxeo/logs/nuxeo_conf.log logs/$h/ || true
-    (( count++ ))
 done
 
 # Add some info to the data stat file
 echo "dbprofile: \"$dbprofile\"" >> $DATA_FILE
 echo "benchid: \"$benchid\"" >> $DATA_FILE
 echo "benchname: \"$benchname\"" >> $DATA_FILE
-echo "nuxeonodes: $count" >> DATA_FILE
+echo "nuxeonodes: $nbnodes" >> DATA_FILE
+echo "classifier: $classifier" >> DATA_FILE
+echo "distribution: $distribution" >> DATA_FILE
 # Extract the mass import document per second
 tail -n1 logs/*/perf*.csv | cut -d \; -f3 | LC_ALL=C xargs printf "import_dps: %.1f" >> $DATA_FILE
 
