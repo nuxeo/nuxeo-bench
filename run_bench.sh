29f049e8bacfb45f2afe69bd848fbf45033bc5b3
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-04-04 16:23:22 +0200

    NXBT-1018: download first to prevent redis timeout

--- run_bench.sh
+++ run_bench.sh
@@ -59,7 +59,8 @@ function load_data_into_redis() {
   echo flushdb | redis-cli -n $REDIS_DB
   # redis-cli don't like unbuffered input
   unset PYTHONUNBUFFERED
-  python ./scripts/inject-arbres.py -d | redis-cli -n $REDIS_DB --pipe
+  python ./scripts/inject-arbres.py -d > /dev/null
+  python ./scripts/inject-arbres.py | redis-cli -n $REDIS_DB --pipe
   export PYTHONUNBUFFERED=1
   popd
 }
