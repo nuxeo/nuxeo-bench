d88ad28f8f46f2a0ef07c8c9b2ea59da76d5f281
Benoit Delbosc <bdelbosc@nuxeo.com>
2017-12-04 12:49:23 +0100

    Don't stop or terminate other env that have a bench tag

--- stop_infra.sh
+++ stop_infra.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 . venv/bin/activate
 
-esinstances=$(aws ec2 describe-instances --filters "Name=tag-key,Values=bench" "Name=tag:bench_role,Values=es" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
-nuxeoinstances=$(aws ec2 describe-instances --filters "Name=tag-key,Values=bench" "Name=tag:bench_role,Values=nuxeo" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
+esinstances=$(aws ec2 describe-instances --filters "Name=tag:bench,Values=test" "Name=tag:bench_role,Values=es" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
+nuxeoinstances=$(aws ec2 describe-instances --filters "Name=tag:bench,Values=test" "Name=tag:bench_role,Values=nuxeo" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
 dbinstances=$(aws ec2 describe-instances --filters "Name=tag:bench_role,Values=db" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
 nosqldbinstances=$(aws ec2 describe-instances --filters "Name=tag:bench_role,Values=nosqldb" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
 mgmtinstances=$(aws ec2 describe-instances --filters "Name=tag:bench_role,Values=mgmt" "Name=instance-state-name,Values=running" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
--- terminate_infra.sh
+++ terminate_infra.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 . venv/bin/activate
 
-esinstances=$(aws ec2 describe-instances --filters "Name=tag-key,Values=bench" "Name=tag:bench_role,Values=es" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
-nuxeoinstances=$(aws ec2 describe-instances --filters "Name=tag-key,Values=bench" "Name=tag:bench_role,Values=nuxeo" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
+esinstances=$(aws ec2 describe-instances --filters "Name=tag:bench,Values=test" "Name=tag:bench_role,Values=es" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
+nuxeoinstances=$(aws ec2 describe-instances --filters "Name=tag:bench,Values=test" "Name=tag:bench_role,Values=nuxeo" --region=eu-west-1 --no-paginate --query "Reservations[*].Instances[*].InstanceId" | grep "i-" | tr -d '" ,')
 
 for i in $(echo -n $esinstances" "$nuxeoinstances); do
     aws ec2 terminate-instances --instance-ids $i --region=eu-west-1
