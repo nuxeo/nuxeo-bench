b5bf624bf4e936bcfa0918f5bfc0cde974f082a4
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-05-16 10:27:11 +0200

    bash cosmit

--- run_bench.sh
+++ run_bench.sh
@@ -12,7 +12,7 @@ SCRIPT_BRANCH=master
 REDIS_DB=7
 REPORT_PATH="./reports"
 GAT_REPORT_VERSION=3.0-SNAPSHOT
-GAT_REPORT_JAR=~/.m2/repository/org/nuxeo/tools/gatling-report/$GAT_REPORT_VERSION/gatling-report-$GAT_REPORT_VERSION-capsule-fat.jar
+GAT_REPORT_JAR=~/.m2/repository/org/nuxeo/tools/gatling-report/${GAT_REPORT_VERSION}/gatling-report-${GAT_REPORT_VERSION}-capsule-fat.jar
 GRAPHITE_DASH=http://bench-mgmt.nuxeo.org/dashboard/#nuxeo-bench
 MUSTACHE_TEMPLATE=./report-templates/data.mustache
 # fail on any command error
@@ -21,36 +21,63 @@ set -e
 function find_bench_scripts_branch() {
   local build_dir="$HERE/bin/.build/nuxeo"
   # if the distrib as been build from a branch use the same branch
-  if [ -d $build_dir ]; then
-    pushd $build_dir
+  if [ -d ${build_dir} ]; then
+    pushd ${build_dir}
     SCRIPT_BRANCH=$(git symbolic-ref -q --short HEAD 2>/dev/null) || SCRIPT_BRANCH=$(git describe --tags --exact-match 2>/dev/null) || SCRIPT_BRANCH="master"
     popd
   fi
 }
 
 
-function clone_bench_scripts() {
+function optimized_clone_bench_scripts() {
   echo "Cloning bench script using $SCRIPT_BRANCH"
-  mkdir $SCRIPT_ROOT
-  pushd $SCRIPT_ROOT
+  mkdir ${SCRIPT_ROOT}
+  pushd ${SCRIPT_ROOT}
   git init
   git config core.sparseCheckout true
   echo "$SCRIPT_DIR" > .git/info/sparse-checkout
-  git remote add origin $NUXEO_GIT
-  git pull --depth 10 origin $SCRIPT_BRANCH
+  git remote add origin ${NUXEO_GIT}
+  git pull --depth 10 origin ${SCRIPT_BRANCH}
   popd
 }
 
-function update_bench_scripts() {
+function optimized_update_bench_scripts() {
   echo "Updating bench script using $SCRIPT_BRANCH"
-  pushd $SCRIPT_ROOT
+  pushd ${SCRIPT_ROOT}
   set +e
-  git pull --depth 20 origin $SCRIPT_BRANCH
+  git pull --depth 20 origin ${SCRIPT_BRANCH}
   if [ $? -ne 0 ]; then
     popd
     set -e
     echo "Fail to update bench script, try to clone"
-    rm -rf $SCRIPT_ROOT
+    rm -rf ${SCRIPT_ROOT}
+    clone_bench_scripts
+  else
+    set -e
+    popd
+  fi
+}
+
+function clone_bench_scripts() {
+  echo "Cloning bench script using $SCRIPT_BRANCH"
+  mkdir ${SCRIPT_ROOT}
+  pushd ${SCRIPT_ROOT}
+  git clone ${NUXEO_GIT}
+  git checkout ${SCRIPT_BRANCH}
+  popd
+}
+
+
+function update_bench_scripts() {
+  echo "Updating bench script using $SCRIPT_BRANCH"
+  pushd ${SCRIPT_ROOT}
+  set +e
+  git checkout ${SCRIPT_BRANCH}
+  if [ $? -ne 0 ]; then
+    popd
+    set -e
+    echo "Fail to update bench script, try to clone"
+    rm -rf ${SCRIPT_ROOT}
     clone_bench_scripts
   else
     set -e
@@ -60,43 +87,51 @@ function update_bench_scripts() {
 
 function clone_or_update_bench_scripts() {
   find_bench_scripts_branch
-  if [ -d $SCRIPT_ROOT ]; then
+  if [ -d ${SCRIPT_ROOT} ]; then
     update_bench_scripts
   else
     clone_bench_scripts
   fi
+  pushd ${SCRIPT_ROOT}
+  mvn -nsu install -N
+  popd
 }
 
 function load_data_into_redis() {
   echo "Load bench data into Redis"
-  pushd $SCRIPT_PATH
-  echo flushdb | redis-cli -n $REDIS_DB
+  pushd ${SCRIPT_PATH}
+  echo flushdb | redis-cli -n ${REDIS_DB}
   set +e
   wget -nc https://maven-eu.nuxeo.org/nexus/service/local/repositories/public-releases/content/org/nuxeo/tools/testing/data-test-les-arbres/1.1/data-test-les-arbres-1.1.zip -O data.zip
   unzip -o data.zip
   set -e
   # redis-cli don't like unbuffered input
   unset PYTHONUNBUFFERED
-  cat data-test*.csv | python ./scripts/inject-arbres.py | redis-cli -n $REDIS_DB --pipe
+  cat data-test*.csv | python ./scripts/inject-arbres.py | redis-cli -n ${REDIS_DB} --pipe
   export PYTHONUNBUFFERED=1
   popd
 }
 
 function gatling() {
   if [ -n "$GATLING_2_1" ]; then
-    mvn -nsu test gatling:execute -Pbench -Durl=$TARGET -Dgatling.simulationClass=$@
+    mvn -nsu test gatling:execute -Pbench -Durl=${TARGET} -Dgatling.simulationClass=$@
   else
-    mvn -nsu gatling:test -Pbench -Durl=$TARGET -Dgatling.simulationClass=$@
+    mvn -nsu gatling:test -Pbench -Durl=${TARGET} -Dgatling.simulationClass=$@
   fi
 }
 
 function find_gatling_version() {
   (mvn -nsu dependency:tree | grep gatling-core | grep 2.1.) && export GATLING_2_1="true"
+  if [ -n "$GATLING_2_1" ]; then
+    echo "Gatling 2.1 detected"
+  else
+    echo "Gatling > 2.3 detected"
+  fi
 }
 
 function run_simulations() {
   echo "Run simulations"
-  pushd $SCRIPT_PATH || exit 2
+  pushd ${SCRIPT_PATH} || exit 2
   mvn -nsu clean
   find_gatling_version
   gatling "org.nuxeo.cap.bench.Sim00Setup"
@@ -122,68 +157,68 @@ function run_simulations() {
 }
 
 function download_gatling_report_tool() {
-  if [ ! -f $GAT_REPORT_JAR ]; then
-    mvn -DgroupId=org.nuxeo.tools -DartifactId=gatling-report -Dversion=$GAT_REPORT_VERSION -Dclassifier=capsule-fat -DrepoUrl=http://maven.nuxeo.org/nexus/content/groups/public-snapshot dependency:get
+  if [ ! -f ${GAT_REPORT_JAR} ]; then
+    mvn -DgroupId=org.nuxeo.tools -DartifactId=gatling-report -Dversion=${GAT_REPORT_VERSION} -Dclassifier=capsule-fat -DrepoUrl=http://maven.nuxeo.org/nexus/content/groups/public-snapshot dependency:get
   fi
 }
 
 function build_report() {
   report_root="${1%-*}"
-  if [ -d $report_root ]; then
+  if [ -d ${report_root} ]; then
     report_root = "${report_root}-bis"
   fi
-  mkdir $report_root || true
-  mv $1 $report_root/detail
-  java -jar $GAT_REPORT_JAR -o $report_root/overview -g $GRAPHITE_DASH $report_root/detail/simulation.log
-  find $report_root -name simulation.log -exec gzip {} \;
+  mkdir ${report_root} || true
+  mv $1 ${report_root}/detail
+  java -jar ${GAT_REPORT_JAR} -o ${report_root}/overview -g ${GRAPHITE_DASH} ${report_root}/detail/simulation.log
+  find ${report_root} -name simulation.log -exec gzip {} \;
 }
 
 function build_reports() {
   echo "Building reports"
   download_gatling_report_tool
-  for report in `find $SCRIPT_PATH/target/gatling/results -name simulation.log`; do
-    build_report `dirname $report`
+  for report in `find ${SCRIPT_PATH}/target/gatling/results -name simulation.log`; do
+    build_report `dirname ${report}`
   done
 }
 
 function move_reports() {
   echo "Moving reports"
-  mv $SCRIPT_PATH/target/gatling/results/* $REPORT_PATH
+  mv ${SCRIPT_PATH}/target/gatling/results/* ${REPORT_PATH}
 }
 
 function build_stat() {
   # create a yml file with all the stats
   set -x
-  java -jar $GAT_REPORT_JAR -f -o $REPORT_PATH -n data.yml -t $MUSTACHE_TEMPLATE \
+  java -jar ${GAT_REPORT_JAR} -f -o ${REPORT_PATH} -n data.yml -t ${MUSTACHE_TEMPLATE} \
     -m import,create,createasync,nav,navjsf,search,update,updateasync,bench,crud,crudasync,reindex \
-    $REPORT_PATH/sim10massimport/detail/simulation.log.gz \
-    $REPORT_PATH/sim20createdocuments/detail/simulation.log.gz \
-    $REPORT_PATH/sim25waitforasync/detail/simulation.log.gz \
-    $REPORT_PATH/sim30navigation/detail/simulation.log.gz \
-    $REPORT_PATH/sim30navigationjsf/detail/simulation.log.gz \
-    $REPORT_PATH/sim30search/detail/simulation.log.gz \
-    $REPORT_PATH/sim30updatedocuments/detail/simulation.log.gz \
-    $REPORT_PATH/sim35waitforasync/detail/simulation.log.gz \
-    $REPORT_PATH/sim50bench/detail/simulation.log.gz \
-    $REPORT_PATH/sim50crud/detail/simulation.log.gz \
-    $REPORT_PATH/sim55waitforasync/detail/simulation.log.gz \
-    $REPORT_PATH/sim80reindexall/detail/simulation.log.gz
-  echo "build_number: $BUILD_NUMBER" >> $REPORT_PATH/data.yml
-  echo "build_url: \"$BUILD_URL\"" >> $REPORT_PATH/data.yml
-  echo "job_name: \"$JOB_NAME\"" >> $REPORT_PATH/data.yml
-  echo "dbprofile: \"$dbprofile\"" >> $REPORT_PATH/data.yml
-  echo "bench_suite: \"$benchsuite\"" >> $REPORT_PATH/data.yml
-  echo "nuxeonodes: $nbnodes" >> $REPORT_PATH/data.yml
-  echo "classifier: \"$classifier\"" >> $REPORT_PATH/data.yml
-  echo "distribution: \"$distribution\"" >> $REPORT_PATH/data.yml
-  echo "default_category: \"$category\"" >> $REPORT_PATH/data.yml
-  echo "" >> $REPORT_PATH/data.yml
+    ${REPORT_PATH}/sim10massimport/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim20createdocuments/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim25waitforasync/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim30navigation/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim30navigationjsf/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim30search/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim30updatedocuments/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim35waitforasync/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim50bench/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim50crud/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim55waitforasync/detail/simulation.log.gz \
+    ${REPORT_PATH}/sim80reindexall/detail/simulation.log.gz
+  echo "build_number: $BUILD_NUMBER" >> ${REPORT_PATH}/data.yml
+  echo "build_url: \"$BUILD_URL\"" >> ${REPORT_PATH}/data.yml
+  echo "job_name: \"$JOB_NAME\"" >> ${REPORT_PATH}/data.yml
+  echo "dbprofile: \"$dbprofile\"" >> ${REPORT_PATH}/data.yml
+  echo "bench_suite: \"$benchsuite\"" >> ${REPORT_PATH}/data.yml
+  echo "nuxeonodes: $nbnodes" >> ${REPORT_PATH}/data.yml
+  echo "classifier: \"$classifier\"" >> ${REPORT_PATH}/data.yml
+  echo "distribution: \"$distribution\"" >> ${REPORT_PATH}/data.yml
+  echo "default_category: \"$category\"" >> ${REPORT_PATH}/data.yml
+  echo "" >> ${REPORT_PATH}/data.yml
   set +x
 }
 
 function clean() {
-  rm -rf $REPORT_PATH || true
-  mkdir $REPORT_PATH
+  rm -rf ${REPORT_PATH} || true
+  mkdir ${REPORT_PATH}
 }
 
 # -------------------------------------------------------
