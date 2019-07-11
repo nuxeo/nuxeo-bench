420d6b1ff48053fdb90a13a34261e080bc868cee
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-07-11 13:00:12 +0200

    Try to rebuild the base AMI with an updated 16.04

--- AMI/build.sh
+++ AMI/build.sh
@@ -1,20 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -e
+set -x
 
 cd $(dirname $0)
 . aws.ini
 
 # Prepare instance
-instance=$(aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type ${TYPE} --key-name ${KEYNAME} --subnet-id ${SUBNET} --security-group-ids ${SECGROUP} --region ${REGION} --user-data file://$(pwd)/bootstrap.sh --query "Instances[0].InstanceId")
-#instance=$(aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type ${TYPE} --key-name ${KEYNAME} --security-group-ids ${SECGROUP} --region ${REGION} --user-data file://$(pwd)/bootstrap.sh --query "Instances[0].InstanceId")
+instance=$(okta-aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type ${TYPE} --key-name ${KEYNAME} --subnet-id ${SUBNET} --region ${REGION} --user-data file://$(pwd)/bootstrap.sh --query "Instances[0].InstanceId")
+#--security-group-ids ${SECGROUP}
+#instance=$(okta-aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type ${TYPE} --key-name ${KEYNAME} --security-group-ids ${SECGROUP} --region ${REGION} --user-data file://$(pwd)/bootstrap.sh --query "Instances[0].InstanceId")
 instance_id=$(echo -n $instance | tr -d '"')
 
 # Wait for shutdown
-while [ "$(aws ec2 describe-instances --instance-ids ${instance_id} --region ${REGION} --query "Reservations[0].Instances[0].State.Name")" != "\"stopped\"" ]; do sleep 10; done
+while [ "$(okta-aws ec2 describe-instances --instance-ids ${instance_id} --region ${REGION} --query "Reservations[0].Instances[0].State.Name")" != "\"stopped\"" ]; do sleep 10; done
 
 # Create image
 tstamp=$(date +"%Y%m%d%H%M")
-aws ec2 create-image --instance-id ${instance_id} --name "nuxeo-base-$tstamp" --description "Nuxeo base (Ubuntu + Nuxeo dependencies)" --region ${REGION}
+okta-aws ec2 create-image --instance-id ${instance_id} --name "nuxeo-base-$tstamp" --description "Nuxeo-base"  --region ${REGION}
 
 # Terminate base instance
-aws ec2 terminate-instances --instance-ids ${instance_id} --region ${REGION}
+okta-aws ec2 terminate-instances --instance-ids ${instance_id} --region ${REGION}
 
